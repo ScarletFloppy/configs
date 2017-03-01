0fa2345b935f74c95d45b75c8084ab71a5069059
Eric Niconovich Jr <eric@Silver.local>
2017-03-01 09:13:46 -0500

    Adding force

--- rancher/install.sh
+++ rancher/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/ScarletFloppy/configs/master/rancher/cloud-config.yaml
-sudo ros install -c $(pwd)/cloud-config.yml -d /dev/sda
+sudo ros install -f -c $(pwd)/cloud-config.yml -d /dev/sda
