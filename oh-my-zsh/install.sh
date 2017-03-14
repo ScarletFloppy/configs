d07f63249d6cc1e7f0e7134268866aa6f92a448b
Eric Niconovich Jr <eric@Silver.local>
2017-03-14 14:37:02 -0400

    check for themes
    Should also check for themes

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/master/oh-my-zsh/zshrc > ~/.zshrc)"
+[ -d ~/.oh-my-zsh/themes ] || mkdir -p ~/.oh-my-zsh/themes
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/master/oh-my-zsh/custom-lambda.zsh-theme > ~/oh-my-zsh/themes/custom-lambda.zsh-theme)"
 sudo echo "/bin/zsh" >> /etc/shells
 sudo echo "/usr/local/bin/zsh" >> /etc/shells
