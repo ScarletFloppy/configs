e61f2b4e375fe8c37ce4f22e0ad4290ba19ff2a1
Eric Niconovich Jr <eric@Silver.local>
2017-03-14 14:33:07 -0400

    Placing oh-my-zsh install last
    This should fix weird script issues

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/master/oh-my-zsh/zshrc > ~/.zshrc)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/master/oh-my-zsh/custom-lambda.zsh-theme > ~/oh-my-zsh/themes/custom-lambda.zsh-theme)"
 sudo echo "/bin/zsh" >> /etc/shells
 sudo echo "/usr/local/bin/zsh" >> /etc/shells
 sudo chsh -s /usr/local/bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 export SETUP_OH-MY-ZSH=true
 exit 0
