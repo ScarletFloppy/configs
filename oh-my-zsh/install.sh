966e00275b7143ca05ae250fbea0cba9a18e076d
Eric Niconovich Jr <eric@Silver.local>
2017-03-14 14:34:58 -0400

    Testing with csh not as root
    this should change the shell only for current user.

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,7 +3,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/mast
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/master/oh-my-zsh/custom-lambda.zsh-theme > ~/oh-my-zsh/themes/custom-lambda.zsh-theme)"
 sudo echo "/bin/zsh" >> /etc/shells
 sudo echo "/usr/local/bin/zsh" >> /etc/shells
-sudo chsh -s /usr/local/bin/zsh
+chsh -s /usr/local/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 export SETUP_OH-MY-ZSH=true
 exit 0
