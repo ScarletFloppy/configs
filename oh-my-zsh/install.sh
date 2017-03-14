7a933b304e5988745b719387cd23f1ad86ba0e4f
Eric Niconovich Jr <eric@Silver.local>
2017-03-14 14:43:28 -0400

    Trying to fix export as well as shells

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -2,9 +2,9 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/master/oh-my-zsh/zshrc > ~/.zshrc)"
 [ -d ~/.oh-my-zsh/themes ] || mkdir -p ~/.oh-my-zsh/themes
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/master/oh-my-zsh/custom-lambda.zsh-theme > ~/oh-my-zsh/themes/custom-lambda.zsh-theme)"
-sudo echo "/bin/zsh" >> /etc/shells
-sudo echo "/usr/local/bin/zsh" >> /etc/shells
+sudo bash -c "echo "/bin/zsh" >> /etc/shells"
+sudo bash -c "echo "/usr/local/bin/zsh" >> /etc/shells"
 chsh -s /usr/local/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-export set SETUP_OH-MY-ZSH="true"
+export set SETUP_OH_MY_ZSH="true"
 exit 0
