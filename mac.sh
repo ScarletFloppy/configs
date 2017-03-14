19e5c2167247af268ad4f213ec756c6517cc2b02
Eric Niconovich Jr <eric@Silver.local>
2017-03-14 14:15:46 -0400

    Updating to oh-my-zsh type

--- mac.sh
+++ mac.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install zsh zsh-completions
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sh -c "$(curl -fsSL https://raw.github.com/ScarletFloppy/configs/zsh/zshrc > ~/.zshrc)"
-sh -c "$(curl -fsSL https://raw.github.com/ScarletFloppy/configs/zsh/custom-lambda.zsh-theme > ~/oh-my-zsh/themes/custom-lambda.zsh-theme)"
-sudo echo "/bin/zsh" >> /etc/shells
-sudo echo "/usr/local/bin/zsh" >> /etc/shells
-sudo chsh -s /usr/local/bin/zsh
+sh -c "$(curl -fsSL https://raw.github.com/ScarletFloppy/configs/oh-my-zsh/install.sh)"
 defaults write com.apple.Terminal "Default Window Settings" Pro
 exit 0
---
+++ oh-my-zsh/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ScarletFloppy/configs/oh-my-zsh/zshrc > ~/.zshrc)"
+sh -c "$(curl -fsSL https://raw.github.com/ScarletFloppy/configs/oh-my-zsh/custom-lambda.zsh-theme > ~/oh-my-zsh/themes/custom-lambda.zsh-theme)"
+sudo echo "/bin/zsh" >> /etc/shells
+sudo echo "/usr/local/bin/zsh" >> /etc/shells
+sudo chsh -s /usr/local/bin/zsh
+export SETUP_OH-MY-ZSH=true
+exit 0
