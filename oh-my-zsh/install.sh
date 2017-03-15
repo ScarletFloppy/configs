213e72615ed5b82de5fd25642875d8d05ccd6e48
Eric Niconovich Jr <eric@Silver.local>
2017-03-15 15:26:34 -0400

    Fixing quotes

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -5,8 +5,8 @@ branch='master'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/$branch/$sname/zshrc > ~/.zshrc)"
 [ -d $themes ] || mkdir -p $themes
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/$branch/$sname/custom-lambda.zsh-theme > $themes/custom-lambda.zsh-theme)"
-sudo bash -c "echo "/bin/zsh" >> /etc/shells"
-sudo bash -c "echo "/usr/local/bin/zsh" >> /etc/shells"
+sudo bash -c 'echo "/bin/zsh" >> /etc/shells'
+sudo bash -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
 chsh -s /usr/local/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/$sname/master/tools/install.sh)"
 export set SETUP_OH_MY_ZSH="true"
