b605cb50111d8fdbfea689d3b6b468e151bba19e
Eric Niconovich Jr <eric@Silver.local>
2017-03-15 15:59:10 -0400

    Adding prompts

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+echo "v1.0 | oh-my-zsh/install.sh"
 sname='oh-my-zsh'
 themes=~/.$sname/themes
 branch='master'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/$branch/$sname/zshrc > ~/.zshrc)"
 [ -d $themes ] || mkdir -p $themes
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/$branch/$sname/custom-lambda.zsh-theme > $themes/custom-lambda.zsh-theme)"
+echo "Changing default shells, password usually required"
 sudo bash -c 'echo "/bin/zsh" >> /etc/shells'
 sudo bash -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
 chsh -s /usr/local/bin/zsh
