10ae3136b2028f08f94504d88d1b4f7a99bb73a8
Eric Niconovich Jr <eric@Silver.local>
2017-03-15 13:58:54 -0400

    Changed something

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/master/oh-my-zsh/zshrc > ~/.zshrc)"
-[ -d ~/.oh-my-zsh/themes ] || mkdir -p ~/.oh-my-zsh/themes
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/master/oh-my-zsh/custom-lambda.zsh-theme > ~/oh-my-zsh/themes/custom-lambda.zsh-theme)"
+sname='oh-my-zsh'
+themes=~/$sname/themes
+branch='master'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/$branch/$sname/zshrc > ~/.zshrc)"
+[ -d $themes ] || mkdir -p $themes
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ScarletFloppy/configs/$branch/$sname/custom-lambda.zsh-theme > ~/$sname/themes/custom-lambda.zsh-theme)"
 sudo bash -c "echo "/bin/zsh" >> /etc/shells"
 sudo bash -c "echo "/usr/local/bin/zsh" >> /etc/shells"
 chsh -s /usr/local/bin/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/$sname/master/tools/install.sh)"
 export set SETUP_OH_MY_ZSH="true"
 exit 0
